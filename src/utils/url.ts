1560f031a959b53f4bfcc47b7bbeb16a2e8b4db2
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-03-26 12:16:29 +0700

    add getUrlType function in url utils

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -29,3 +29,34 @@ export function urlChecker(url: string): ResultType {
     }
   }
 }
+
+export function getUrlType(url: string): UrlType {
+  if (url === "") {
+    return UrlType.Empty;
+  }
+
+  const isValid = urlValidator(url);
+  if (!isValid) {
+    return UrlType.Invalid;
+  }
+
+  try {
+    const { type } = getUrl(url);
+    switch (type) {
+      case "file":
+        return UrlType.File;
+
+      case "folder":
+        return UrlType.Folder;
+
+      default:
+        return UrlType.NotFound;
+    }
+  } catch (err) {
+    if (err instanceof Error && err.message === "Not Found") {
+      return UrlType.NotFound;
+    }
+  }
+
+  return UrlType.NotFound;
+}
