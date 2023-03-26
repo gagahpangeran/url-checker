1826ccfeff3759f69b2efccc7247434bd5bf15f3
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-03-26 15:20:56 +0700

    update index file to use createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import App from "./app";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import "./styles/index.scss";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
