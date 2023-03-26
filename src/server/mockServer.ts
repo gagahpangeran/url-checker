6b086bcea32bf5d6724b87ef7f5b65a5a7fe4c0b
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-03-26 11:27:31 +0700

    add simple mock server

---
+++ src/server/mockServer.ts
@@ -0,0 +1,25 @@
+interface UrlType {
+  url: string;
+  type: "file" | "folder";
+}
+
+const urlList: UrlType[] = [
+  {
+    url: "https://example.com/file.pdf",
+    type: "file"
+  },
+  {
+    url: "http://example.com/folder/",
+    type: "folder"
+  }
+];
+
+export default function getUrl(url: string) {
+  const result = urlList.find(u => u.url === url);
+
+  if (result === undefined) {
+    throw Error("Not Found");
+  }
+
+  return result;
+}
