f5e52fcd93714d4a009e9423cc2ca8e8c458dda6
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-03-26 10:16:53 +0700

    add more test for url validator

--- src/utils/validator.test.ts
+++ src/utils/validator.test.ts
@@ -1,13 +1,23 @@
 import { urlValidator } from "./validator";
 
-describe("Test URL validaator", () => {
-  it("should accept valid URL", () => {
+describe("Test URL validator", () => {
+  it("should accept URL starts with https", () => {
     const result = urlValidator("https://gagahpangeran.com/");
+    expect(result).toBe(true);
+  });
+
+  it("should accept URL starts with http", () => {
+    const result = urlValidator("http://gagahpangeran.com/");
     expect(result).toBe(true);
   });
 
   it("should reject invalid URL", () => {
     const result = urlValidator("something");
+    expect(result).toBe(false);
+  });
+
+  it("should reject URL other than http or https", () => {
+    const result = urlValidator("mailto:gpr@gagahpangeran.com");
     expect(result).toBe(false);
   });
 });
